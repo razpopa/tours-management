6636ce7f0293fa934beea78d3eaedd79704ab512
Razvan <razvan.popa@accesa.eu>
2021-08-06 23:59:56 +0300

    Updated UI

---
+++ ToursManagement/ToursManagement/Views/ScheduleView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToursManagement.Views
+{
+	/// <summary>
+	/// Interaction logic for ScheduleView.xaml
+	/// </summary>
+	public partial class ScheduleView : UserControl
+	{
+		public ScheduleView()
+		{
+			InitializeComponent();
+		}
+	}
+}
